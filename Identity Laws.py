ff1d1023e3d24c3e3165e8998c988d07b92bdc84
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-05-17 22:44:57 +0800

    Initial commit

---
+++ Identity Laws.py
@@ -0,0 +1,19 @@
+#logical Equivalence
+#TODO--------------- [ VARIABLES ] --------------------#
+p = True
+
+#!------------------- [ METHODS ] ---------------------#
+def identity_law1(p):
+    return p and True
+
+def identity_law2(p):
+    return p or False
+
+#?--------------------- [ MAIN ] ----------------------#
+
+Identity_1 = identity_law1(p)
+Identity_2 = identity_law2(p)
+
+print(" ")
+print(f"P and True [{Identity_1}] = p [{p}]")
+print(f"P OR False [{Identity_2}] = p [{p}]")
