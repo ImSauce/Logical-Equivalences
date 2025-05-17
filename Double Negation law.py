eb0fccd7eb592e961d679c2cff156c4c5fb68216
Im Sauce <130982799+ImSauce@users.noreply.github.com>
2025-05-17 22:58:21 +0800

    3

---
+++ Double Negation law.py
@@ -0,0 +1,15 @@
+#logical Equivalence
+#Double Negation law
+#TODO--------------- [ VARIABLES ] --------------------#
+p = True
+
+#!------------------- [ METHODS ] ---------------------#
+
+
+#?--------------------- [ MAIN ] ----------------------#
+not_p = not p
+DN = not not_p
+
+
+print(" ")
+print(f"-(-p) [{DN}] = p [{p}]")
